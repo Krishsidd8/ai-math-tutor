40765c2055e0337470e88e707649dc1321f2750d
Krishsidd8 <krishsidd8@gmail.com>
2025-08-03 09:03:45 -0700

    corrected ocr_checkpoint.pt link download

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
+import os
 import torch
+import gdown
 
 device = torch.device("cpu")
 checkpoint_path = "ocr_checkpoint.pt"
+
+if not os.path.exists(checkpoint_path):
+    print("Downloading model checkpoint from Google Drive...")
+    gdown.download(
+        "https://drive.google.com/uc?id=1IttUFMaSxgyEbunjwvnOntFtcWWWuQdh",
+        checkpoint_path,
+        quiet=False
+    )
+    print("Model downloaded successfully.")
