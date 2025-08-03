d769de6b3de50b3ec195c06454294b58e82d6cc8
Krishsidd8 <krishsidd8@gmail.com>
2025-08-02 20:57:45 -0700

    run method change

--- app.py
+++ app.py
@@ -94,9 +94,3 @@ def solve_equation():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-# ============ Run =================== #
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
